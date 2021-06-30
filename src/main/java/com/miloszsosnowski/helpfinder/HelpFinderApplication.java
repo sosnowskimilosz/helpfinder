e8dd4af36e4ac30980b209e9a43454d55cc2e7b9
sosnowskimilosz <sosnowskimilosz@gmail.com>
2021-06-30 12:20:37 +0200

    initial commit

---
+++ src/main/java/com/miloszsosnowski/helpfinder/HelpFinderApplication.java
@@ -0,0 +1,13 @@
+package com.miloszsosnowski.helpfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miloszsosnowski/helpfinder/HelpFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miloszsosnowski.helpfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
